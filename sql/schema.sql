5bb860de31bb856f78f1cd4f149e23387688a47e
michael Means <colemeans51@gmail.com>
2022-02-09 17:50:22 -0600

    create table role

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,3 +9,11 @@ CREATE TABLE department (
   name VARCHAR(45) NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
