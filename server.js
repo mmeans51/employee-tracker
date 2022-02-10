b5050f5c87a30ec6a273c5f2ede3f613e0052867
michael Means <colemeans51@gmail.com>
2022-02-10 17:09:35 -0600

    connect function and call firstPrompt

---
+++ server.js
@@ -0,0 +1,27 @@
+//dependencies required
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+
+//mysql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // Your password
+    password: 'PlacePassWordHere',
+    database: 'employeesDB'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    
+    // runs the app
+    firstPrompt();
+});
