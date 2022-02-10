6cd1b368b02b76e11c4effb94c2084fec44b6e8e
michael Means <colemeans51@gmail.com>
2022-02-09 18:04:40 -0600

    department values

---
+++ sql/seed.sql
@@ -0,0 +1,10 @@
+USE employeesDB;
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
