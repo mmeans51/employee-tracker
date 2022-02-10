39f0194c0715281135ac012e38c623f7938b4976
michael Means <colemeans51@gmail.com>
2022-02-10 17:50:15 -0600

    update firstPrompt function

--- server.js
+++ server.js
@@ -25,3 +25,53 @@ connection.connect(function (err) {
     // runs the app
     firstPrompt();
 });
+
+// function which prompts the user for what action they should take
+function firstPrompt() {
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+          "View Employees",
+          "View Employees by Department",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Add Role",
+          "End"]
+      })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployee();
+            break;
+  
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
+        
+          case "Add Employee":
+            addEmployee();
+            break;
+  
+          case "Remove Employees":
+            removeEmployees();
+            break;
+  
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+  
+          case "Add Role":
+            addRole();
+            break;
+  
+          case "End":
+            connection.end();
+            break;
+        }
+      });
+  }
