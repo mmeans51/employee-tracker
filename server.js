ef4b14cadaeaba615022dc077071576dd505fdfa
michael Means <colemeans51@gmail.com>
2022-02-10 18:27:35 -0600

    tion viewEmployee

--- server.js
+++ server.js
@@ -75,3 +75,18 @@ function firstPrompt() {
         }
       });
   }
+
+
+  //View Employees/ READ all, SELECT * FROM
+function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
