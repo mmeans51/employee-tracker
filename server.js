498feaa0f30858920f9042ea564133812a951131
michael Means <colemeans51@gmail.com>
2022-02-10 19:09:04 -0600

    viewemployeebyDepartment()

--- server.js
+++ server.js
@@ -101,3 +101,19 @@ function viewEmployee() {
       });
     
     }
+
+  
+function viewEmployeeByDepartment() {
+    console.log("Viewing employees by department\n");
+  
+    var query =
+      `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
