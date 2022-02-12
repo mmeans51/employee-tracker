55ddd21b0e3b5692b18110a7256273fab1b16e5f
michael Means <colemeans51@gmail.com>
2022-02-11 18:52:50 -0600

    make employee array

--- server.js
+++ server.js
@@ -163,3 +163,25 @@ function promptDepartment(departmentChoices) {
       });
     });
 }
+
+// Make a employee array
+function addEmployee() {
+  console.log("Inserting an employee!")
+
+  var query =
+    `SELECT r.id, r.title, r.salary 
+      FROM role r`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const roleChoices = res.map(({ id, title, salary }) => ({
+      value: id, title: `${title}`, salary: `${salary}`
+    }));
+
+    console.table(res);
+    console.log("RoleToInsert!");
+
+    promptInsert(roleChoices);
+  });
+}
